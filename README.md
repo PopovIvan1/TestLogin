8e616a08a08ff043b1d0dd6b1b0f0f4840e6e160
Popov Ivan <bottob79@mail.ru>
2023-01-13 17:47:48 +0300

    Added main program functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+Create a 1-page site using Bootstrap which will contain two input fields (login, password) and submit button.
+On submit, call login function from our web service (WSDL SOAP)
+http://isapi.icu-tech.com/icutech-test.dll 
+If login successful, display green success message and entity details.
+If error, display a red error message.
+To create a test account, use the RegisterNewCustomer function.
+After functionality is complete, the page must pass google page speed test. The result must be for mobile 100/100 for desktop minimum 90 level. https://developers.google.com/speed/pagespeed/insights/ 
+Mobile speed should be 3 second maximum, https://testmysite.withgoogle.com/intl/en-gb 
---
+++ TestLogin/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace TestLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<string> TryLogin(string userName, string password)
+        {
+            ICUTechClient client = new ICUTechClient();
+            LoginResponse response = await client.LoginAsync(userName, password, "");
+            return convertMessage(response.@return);
+        }
+
+        private string convertMessage(string message)
+        {
+            Dictionary<string, object> messageToArray = JsonSerializer.Deserialize<Dictionary<string, object>>(message);
+            string color = messageToArray.Values.First().ToString() == "-1" ? "red" : "green";
+            string convertedMessage = $"<i style=\"color: {color}\">";
+            foreach (var atribute in messageToArray)
+            {
+                convertedMessage += atribute.Key + ": " + atribute.Value.ToString() + "<br>";
+            }
+            convertedMessage += "</i>";
+            return convertedMessage;
+        }
+    }
+}
